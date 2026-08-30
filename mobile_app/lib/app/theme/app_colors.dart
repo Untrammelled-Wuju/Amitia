916139b70f0508cc311f2425ffc4a70acadb804d
Untrammelled <3151508592@qq.com>
2026-08-31 02:02:12 +0800

    feat: 应用主题系统配色方案及抽屉路由状态管理优化

--- mobile_app/lib/app/theme/app_colors.dart
+++ mobile_app/lib/app/theme/app_colors.dart
@@ -17,27 +17,27 @@ class AppColors {
 class _LightColors {
   const _LightColors();
 
-  final Color backgroundPrimary = const Color(0xFFFFFFFF);
-  final Color backgroundSecondary = const Color(0xFFF5F5F5);
-  final Color surfacePrimary = const Color(0xFFFFFFFF);
-  final Color surfaceSecondary = const Color(0xFFF8F8F8);
+  final Color backgroundPrimary = const Color(0xFFF8F6F2);
+  final Color backgroundSecondary = const Color(0xFFF7F4EF);
+  final Color surfacePrimary = const Color(0xFFFFFDF9);
+  final Color surfaceSecondary = const Color(0xFFF2EEE8);
 
   final Color accentPrimary = const Color(0xFF8A5728);
-  final Color accentSecondary = const Color(0xFFA67547);
-  final Color accentSoft = const Color(0xFFF1E4D4);
-  final Color accentPressed = const Color(0xFF5E3516);
+  final Color accentSecondary = const Color(0xFF6E421F);
+  final Color accentSoft = const Color(0xFFEFE1D2);
+  final Color accentPressed = const Color(0xFF6E421F);
 
-  final Color textPrimary = const Color(0xFF1F2028);
-  final Color textSecondary = const Color(0xFF6F707B);
-  final Color textTertiary = const Color(0xFFA2A3AC);
-  final Color textDisabled = const Color(0xFFC5C6CD);
+  final Color textPrimary = const Color(0xFF24221F);
+  final Color textSecondary = const Color(0xFF67615A);
+  final Color textTertiary = const Color(0xFF9B938A);
+  final Color textDisabled = const Color(0xFFB9B2AA);
 
-  final Color borderPrimary = const Color(0xFFE8E8E8);
-  final Color borderSecondary = const Color(0xFFF0F0F0);
+  final Color borderPrimary = const Color(0xFFE5DED5);
+  final Color borderSecondary = const Color(0xFFEFEAE4);
 
-  final Color success = const Color(0xFF3F7653);
-  final Color warning = const Color(0xFF8A5A18);
-  final Color error = const Color(0xFFA83F3F);
+  final Color success = const Color(0xFF4D715D);
+  final Color warning = const Color(0xFF9A6A31);
+  final Color error = const Color(0xFFA34F49);
   final Color info = const Color(0xFF4E6C82);
 
   final Color scrim = const Color(0x66000000);
@@ -47,27 +47,27 @@ class _LightColors {
 class _DarkColors {
   const _DarkColors();
 
-  final Color backgroundPrimary = const Color(0xFF111217);
-  final Color backgroundSecondary = const Color(0xFF17181E);
-  final Color surfacePrimary = const Color(0xFF1D1E25);
-  final Color surfaceSecondary = const Color(0xFF24252D);
+  final Color backgroundPrimary = const Color(0xFF111214);
+  final Color backgroundSecondary = const Color(0xFF17181A);
+  final Color surfacePrimary = const Color(0xFF191A1C);
+  final Color surfaceSecondary = const Color(0xFF202124);
 
-  final Color accentPrimary = const Color(0xFFC99557);
-  final Color accentSecondary = const Color(0xFFD4AA76);
-  final Color accentSoft = const Color(0x1FC99557);
-  final Color accentPressed = const Color(0xFFAD773D);
+  final Color accentPrimary = const Color(0xFF9C8068);
+  final Color accentSecondary = const Color(0xFFC7AD96);
+  final Color accentSoft = const Color(0xFF2C2825);
+  final Color accentPressed = const Color(0xFFC7AD96);
 
-  final Color textPrimary = const Color(0xFFF2F2F5);
-  final Color textSecondary = const Color(0xFFB1B2BB);
-  final Color textTertiary = const Color(0xFF7E7F89);
-  final Color textDisabled = const Color(0xFF4A4B52);
+  final Color textPrimary = const Color(0xFFF0F0EF);
+  final Color textSecondary = const Color(0xFFB7B6B3);
+  final Color textTertiary = const Color(0xFF85827E);
+  final Color textDisabled = const Color(0xFF5F5E5B);
 
-  final Color borderPrimary = const Color(0xFF2D2E36);
-  final Color borderSecondary = const Color(0xFF25262D);
+  final Color borderPrimary = const Color(0xFF303134);
+  final Color borderSecondary = const Color(0xFF27282B);
 
-  final Color success = const Color(0xFF75A184);
-  final Color warning = const Color(0xFFC99A56);
-  final Color error = const Color(0xFFC96E6A);
+  final Color success = const Color(0xFF7FAF8D);
+  final Color warning = const Color(0xFFC99759);
+  final Color error = const Color(0xFFCC7770);
   final Color info = const Color(0xFF9A9E99);
 
   final Color scrim = const Color(0x99000000);
