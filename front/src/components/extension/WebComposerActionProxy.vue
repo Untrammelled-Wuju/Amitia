2d437eea482dc3797d9d26387efb06df19b5aea0
Untrammelled <3151508592@qq.com>
2026-09-14 14:04:17 +0800

    feat: 前端新增 WebComposerActionProxy.vue 组件

---
+++ front/src/components/extension/WebComposerActionProxy.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { UIContributionSummary } from "@/stores/extensionUI";
+import SandboxWebUIFrame from "./SandboxWebUIFrame.vue";
+
+defineProps<{
+  contribution: UIContributionSummary;
+  context: Record<string, unknown>;
+  slotId: string;
+}>();
+</script>
+
+<template>
+  <div class="web-composer-action-proxy">
+    <SandboxWebUIFrame
+      :contribution="contribution"
+      :context="context"
+      :slot-id="slotId"
+    />
+  </div>
+</template>
+
+<style scoped>
+.web-composer-action-proxy {
+  position: relative;
+  width: 32px;
+  height: 32px;
+  min-width: 32px;
+}
+</style>
