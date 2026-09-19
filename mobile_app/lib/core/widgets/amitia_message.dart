4acf406ca9fc0b4814dda58ac275cc920f29ab69
Untrammelled <3151508592@qq.com>
2026-09-19 21:05:33 +0800

    style(android): compact chat message spacing

--- mobile_app/lib/core/widgets/amitia_message.dart
+++ mobile_app/lib/core/widgets/amitia_message.dart
@@ -302,7 +302,7 @@ class AmitiaMessageBubble extends StatelessWidget {
       padding: EdgeInsets.only(
         left: AppSpacing.lg,
         right: AppSpacing.lg,
-        bottom: 14,
+        bottom: 8,
       ),
       child: Row(
         mainAxisAlignment: isUser
